dea6bdf5ffde59d327b7acca6b4ccaec5b02010c
phi2dao <orgodemir.v@gmail.com>
2013-05-17 13:54:53 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CardChan::Application.config.secret_token = '5d1052355f2ebc548fbcee21cfd1d24fc1e7d6c1cc6d8d1c528d86121fc2ac3abcd0749fd10544505f60272227d0adda3e1042c9fa9e7c9160f9401a922318e8'
