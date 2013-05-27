9975ce4724d80f774a6c842d2c059f38843e97f2
phi2dao <orgodemir.v@gmail.com>
2013-05-27 11:45:14 -0400

    Commit for small fixes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,55 +4,15 @@ class EventsController < ApplicationController
     @event = @deck.events.build params[:event]
     case @event.action
     when /flip|draw/
-      if @event.input.to_i > @deck.cards.length
-        flash[:error] = "Not enough cards in the deck"
-      else
-        cards = []
-        @event.input.to_i.times { cards << @deck.cards.pop }
-        if cards.empty?
-          flash[:error] = "Bad number of cards"
-        else
-          if @event.action == 'flip'
-            @event.output = "Flipped #{cards.to_sentence}."
-          else
-            @deck.hand += cards
-            @event.output = "Drew #{cards.to_sentence}."
-          end
-        end
-      end
+      flip_or_draw @event.input
     when /play|discard/
-      cards = @event.input.split(',').map {|card| card.strip }
-      if (cards & @deck.hand).length != cards.length
-        flash[:error] = pluralize(cards.length - (cards & @deck.hand).length, "invalid card")
-      else
-        cards.each do |card|
-          @deck.hand.delete card
-        end
-        if @event.action == 'play'
-          @event.output = "Played #{cards.to_sentence}."
-        else
-          @event.output = "Discarded #{cards.to_sentence}."
-        end
-      end
+      play_or_discard @event.input
     when 'random'
-      if @event.input.to_i > @deck.hand.length
-        flash[:error] = "Not enough cards in hand"
-      else
-        cards = []
-        @event.input.to_i.times { cards << @deck.hand.delete_at(rand(@deck.hand.length)) }
-        if cards.empty?
-          flash[:error] = "Bad number of cards"
-        else
-          @event.output = "Discarded #{cards.to_sentence} at random."
-        end
-      end
+      discard_at_random @event.input
     when 'shuffle'
-      @deck.cards = get_decks[@deck.deck_type].shuffle - @deck.hand
-      @event.output = "Shuffled the deck."
+      shuffle
     when 'shuffle_all'
-      @deck.hand = []
-      @deck.cards = get_decks[@deck.deck_type].shuffle
-      @event.output = "Shuffled all cards into the deck."
+      shuffle_all
     end
     unless @event.email.downcase == @deck.email
       @event.email = ""
@@ -68,5 +28,90 @@ class EventsController < ApplicationController
 
   def show
     @event = Event.find params[:id]
+  end
+
+  private
+  def flip_or_draw input
+    if is_number? input
+      if input.to_i > get_decks[@deck.deck_type].length - @deck.hand.length
+        flash[:error] = "Can't #{@deck.action} that many cards"
+      else
+        shuffled = false
+        cards1 = []
+        cards2 = []
+        input.to_i.times do
+          card = @deck.cards.pop
+          if card and not shuffled
+            cards1 << card
+          elsif card and shuffled
+            cards2 << card
+          else
+            @deck.cards = get_decks[@deck.deck_type].shuffle - @deck.hand
+            shuffled = true
+          end
+        end
+        if @event.action == 'flip'
+          if shuffled
+            @event.output = "Flipped #{cards1.to_sentence}, shuffled the deck, and then flipped #{cards2.to_sentence}."
+          else
+            @event.output = "Flipped #{cards1.to_sentence}."
+          end
+        else
+          @deck.hand += cards1
+          @deck.hand += cards2
+          if shuffled
+            @event.output = "Drew #{cards1.to_sentence}, shuffled the deck, and then drew #{cards2.to_sentence}."
+          else
+            @event.output = "Drew #{cards1.to_sentence}."
+          end
+        end
+      end
+    end
+  end
+
+  def play_or_discard input
+    cards = @event.input.split(',').map {|card| card.strip }
+    bad_cards = cards.find_all {|card| !@deck.hand.member? card }
+    if bad_cards.any?
+      flash[:error] = "#{pluralize bad_cards.length, "invalid card"}: #{bad_cards.to_sentence}"
+    else
+      cards.each {|card| @deck.hand.delete card }
+      if @event.action == 'play'
+        @event.output = "Played #{cards.to_sentence}."
+      else
+        @event.output = "Discarded #{cards.to_sentence}."
+      end
+    end
+  end
+  
+  def discard_at_random input
+    if is_number? input
+      if input.to_i > @deck.hand.length
+        cards = input.to_i.times.map { @deck.hand.delete_at(rand(@deck.hand.length)) }
+        @event.output = "Discarded #{cards.to_sentence} at random."
+      else
+        flash[:error] = "Not enough cards in hand"
+      end
+    end
+  end
+
+  def shuffle
+    @deck.cards = get_decks[@deck.deck_type].suffle - @deck.hand
+    @event.output = "Shuffled the deck."
+  end
+
+  def shuffle_all
+    @deck.hand = []
+    @deck.cards = get_decks[@deck.deck_type].shuffle
+    @event.output = "Shuffled all cards into the deck."
+  end
+
+  def is_number? input
+    if input.match /\d+/
+      true
+    else
+      flash[:error] = "Input should be a number"
+      false
+    end
   end
 end
